696951df3591a9aeba3b628106299ef0e5ad5b74
VijayaProCoder <99400608+VijayaProCoder@users.noreply.github.com>
2022-02-12 14:51:37 +0530

    Add files via upload

---
+++ odd or even number.py
@@ -0,0 +1,9 @@
+def OddEven(parameter):
+    if(parameter%2 == 0):
+        print(parameter,"is even")
+    else:
+        print(parameter,"is odd")
+OddEven(40)       
+            
+            
+        
