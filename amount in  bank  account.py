9b4eb2e80ba57d6c83978342ad70ddf6d05eadab
VijayaProCoder <99400608+VijayaProCoder@users.noreply.github.com>
2022-02-12 18:16:18 +0530

    Add files via upload

---
+++ amount in  bank  account.py
@@ -0,0 +1,13 @@
+def BankAccount(amount,transaction_mode):
+    netAmount=10000;
+    amount=int(amount)
+    if transaction_mode == "D":
+        netAmount += amount
+    elif transaction_Mode == "W":
+        netAmount -= amount
+    else:
+        pass
+    return netAmount;
+print (BankAccount(100, "D"))
+            
+            
---
+++ program that computes the value in the form of X+XX+XXX+XXXX.py
@@ -0,0 +1,8 @@
+def compute(X):
+    n1=int("%s"%X)
+    n2=int("%s%s"%(X,X))
+    n3=int("%s%s%s"%(X,X,X))
+    n4=int("%s%s%s%s"%(X,X,X,X))
+    print(n1+n2+n3+n4)
+compute (10)
+    
